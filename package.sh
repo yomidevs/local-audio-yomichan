0cf2e0515d943bac355b8cf1f280934c857c0b52
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-02-09 22:39:26 -0700

    made package script work?

--- package.sh
+++ package.sh
@@ -1,2 +1,4 @@
 #zip -r localaudio_sql09__2023_01_15.ankiaddon __init__.py ./make_nhk16_db.py manifest.json meta.json user_files/
-zip -r localaudio_sql09__2023_01_24.ankiaddon __init__.py ./make_nhk16_db.py manifest.json meta.json user_files/
+cd plugin
+zip -r ../localaudio.ankiaddon *.py source/*.py
+cd ..
