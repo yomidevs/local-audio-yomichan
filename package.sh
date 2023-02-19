1a532f6d775ce5f5b0341927e4871583e227b2c3
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-02-19 15:01:17 -0700

    fixed package.sh so it doesn't retain deleted files

--- package.sh
+++ package.sh
@@ -1,4 +1,6 @@
-#zip -r localaudio_sql09__2023_01_15.ankiaddon __init__.py ./make_nhk16_db.py manifest.json meta.json user_files
+rm ./localaudio.ankiaddon
 cd plugin
+mkdir user_files
+touch user_files/.placeholder
 zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder
 cd ..
