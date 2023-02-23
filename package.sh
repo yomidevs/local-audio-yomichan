02c16604cf5a41590b749cf7c996f3bb96e6c133
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-02-23 12:23:42 -0700

    remove manifest from standard package

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 rm ./localaudio.ankiaddon
 cd plugin
-# manifest.json is ignored if downloaded from ankiweb, so it's safe to set the name to whatever
-zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder manifest.json
+#zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder manifest.json
+zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder
 cd ..
