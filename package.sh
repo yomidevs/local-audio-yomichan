6cc8aa0e29f85ad7c97b17256103c09b0b1d8d34
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-03-13 23:28:44 -0600

    chore: added --dev option to package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+# Usage:
+# ./package.sh
+# OR
+# ./package.sh --dev
+
 rm ./localaudio.ankiaddon
 cd plugin
 #zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder manifest.json
 zip -r ../localaudio.ankiaddon *.py source/*.py version.txt user_files/.placeholder
+
+if [[ "$1" == "--dev" ]]; then
+    # update zip with manifest
+    zip -r ../localaudio.ankiaddon manifest.json
+fi
+
 cd ..
